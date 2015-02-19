35ea89878a4a2ca6a02ece31f2caa31d16664098
Tan Janan <janan1990@gmail.com>
2015-02-20 05:26:21 +0800

    initial commit

---
+++ data/exp_Townhall.js
@@ -0,0 +1,10 @@
+var exp_Townhall = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#749e3d', "radius_qgis2leaf": 4.0, "borderColor_qgis2leaf": '#000000', "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "id": 258.000000, "osm_id": 1565963296.000000, "name": "Makespace Singapore", "type": "townhall", "icon_exp": "townhal.svg" }, "geometry": { "type": "Point", "coordinates": [ 103.86189446953784, 1.309592533736792 ] } },
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#749e3d', "radius_qgis2leaf": 4.0, "borderColor_qgis2leaf": '#000000', "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "id": 336.000000, "osm_id": 1830346332.000000, "name": "Bukit Merah Branch Office", "type": "townhall", "icon_exp": "townhal.svg" }, "geometry": { "type": "Point", "coordinates": [ 103.81761966441371, 1.283127596475749 ] } },
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#749e3d', "radius_qgis2leaf": 4.0, "borderColor_qgis2leaf": '#000000', "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "id": 373.000000, "osm_id": 2299105274.000000, "name": "Holland-Bukit Panjang Town Council", "type": "townhall", "icon_exp": "townhal.svg" }, "geometry": { "type": "Point", "coordinates": [ 103.77291302363813, 1.379062082514997 ] } }
+]
+}
---
+++ data/exp_University.js
@@ -0,0 +1,9 @@
+var exp_University = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#9b21f8', "radius_qgis2leaf": 4.0, "borderColor_qgis2leaf": '#000000', "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "id": 388.000000, "osm_id": 2440441937.000000, "name": "Lee Kong Chian School of Medicine (Yunan Campus)", "type": "university", "icon_exp": "university.svg" }, "geometry": { "type": "Point", "coordinates": [ 103.67939436973154, 1.343378392867614 ] } },
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#9b21f8', "radius_qgis2leaf": 4.0, "borderColor_qgis2leaf": '#000000', "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "id": 389.000000, "osm_id": 2440447194.000000, "name": "Lee Kong Chian School of Medicine (Novena Campus)", "type": "university", "icon_exp": "university.svg" }, "geometry": { "type": "Point", "coordinates": [ 103.84895725344661, 1.321226531522589 ] } }
+]
+}
